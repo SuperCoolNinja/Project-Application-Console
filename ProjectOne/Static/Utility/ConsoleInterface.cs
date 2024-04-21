99a3bd32f3ccdce2c712b50d8b47dbaa3205eb3c
SuperCoolNinja <sbpro570@gmail.com>
2024-04-21 11:34:00 +0200

    added new class for constraint InputValidator and added student creation

--- ProjectOne/Static/Utility/ConsoleInterface.cs
+++ ProjectOne/Static/Utility/ConsoleInterface.cs
@@ -1,4 +1,6 @@
 ﻿using ProjectOne.Entities;
+using static System.Formats.Asn1.AsnWriter;
+using System;
 
 namespace ProjectOne.Static.Utility;
 
@@ -46,8 +48,65 @@ internal static class ConsoleInterface
         Console.Clear();
 
         Console.WriteLine($"List of student :\n");
-        
+
         foreach (Student student in students)
             Console.WriteLine($"\tId : {student.Id}\n\tName : {student.FirstName} {student.LastName}\n\tBirthday : {student.Birthday}\n\n");
+    }
+
+
+    /// <summary>
+    /// Asks for student info and returns it as a tuple.
+    /// </summary>
+    /// <returns>
+    /// A tuple containing three strings:
+    /// - firstname: The student's first name.
+    /// - lastname: The student's last name.
+    /// - birthday: The student's birthday in the format "MM/DD/YYYY".
+    /// </returns>
+    public static (string? firstname, string? lastname, string? birthday) AskStudentInfo()
+    {
+        string? firstName = null;
+        string? lastName = null;
+        string? birthday = null; ;
+        bool isAnyFieldEmpty, doNamesContainDigits, isBirthdayInvalid;
+
+        do
+        {
+            Console.WriteLine("\n\nEnter student details: [type exit to go back to main menu]");
+            Console.Write("Firstname: ");
+            firstName = Console.ReadLine();
+
+            if (firstName == "exit")
+                break;
+
+            Console.Write("Lastname: ");
+            lastName = Console.ReadLine();
+            if (lastName == "exit")
+                break;
+
+
+            Console.Write("Birthday (Format: DD/MM/YYYY): ");
+            birthday = Console.ReadLine();
+            if (birthday == "exit")
+                break;
+
+            isAnyFieldEmpty = InputValidator.AnyNullOrEmpty(firstName, lastName, birthday);
+            doNamesContainDigits = InputValidator.ContainsDigits(firstName, lastName);
+            isBirthdayInvalid = !InputValidator.IsValidDate(birthday);
+
+
+
+            if (isAnyFieldEmpty)
+                Console.WriteLine("No fields can be empty.");
+
+            if (doNamesContainDigits)
+                Console.WriteLine("Names should not contain digits.");
+
+            if (isBirthdayInvalid)
+                Console.WriteLine("Birthday must be in the correct format (DD/MM/YYYY) and a valid date.");
+
+        } while (isAnyFieldEmpty || doNamesContainDigits || isBirthdayInvalid);
+
+        return (firstName, lastName, birthday);
     }
 }
---
+++ ProjectOne/Static/Utility/InputValidator.cs
@@ -0,0 +1,66 @@
+﻿namespace ProjectOne.Static.Utility;
+
+internal class InputValidator
+{
+    /// <summary>
+    /// Validates that the provided string is a valid date in the DD/MM/YYYY format.
+    /// </summary>
+    /// <param name="dateStr">The date string to validate.</param>
+    /// <returns>True if the date is valid and in the specified format, otherwise false.</returns>
+    public static bool IsValidDate(string dateStr)
+    {
+        bool didParsedData = false;
+
+        try
+        {
+            DateTime parsedDate;
+            didParsedData = DateTime.TryParseExact(dateStr, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out parsedDate);
+        }
+        catch (Exception ex)
+        {
+            Logger.Write($"[InputValidator] - {ex.Message}");
+        }
+
+        return didParsedData;
+    }
+
+    /// <summary>
+    /// Checks if any of the provided strings are null or empty.
+    /// </summary>
+    /// <param name="inputs">Strings to check.</param>
+    /// <returns>True if any string is null or empty, otherwise false.</returns>
+    public static bool AnyNullOrEmpty(params string[] inputs)
+    {
+        bool isNullOrEmpty = false;
+        try
+        {
+            isNullOrEmpty = inputs.Any(string.IsNullOrEmpty) || inputs.Any(input => input == "exit");
+        }
+        catch (Exception ex)
+        {
+            Logger.Write($"[InputValidator] - {ex.Message}");
+        }
+
+        return isNullOrEmpty;
+    }
+
+    /// <summary>
+    /// Checks if any of the provided strings contain digits.
+    /// </summary>
+    /// <returns>True if any of the provided strings contain any digits, otherwise false.</returns>
+    public static bool ContainsDigits(params string[] inputs)
+    {
+        bool doesContainDigit = false;
+
+        try
+        {
+            doesContainDigit = inputs.Any(input => input.Any(char.IsDigit));
+        }
+        catch (Exception ex)
+        {
+            Logger.Write($"[InputValidator] - {ex.Message}");
+        }
+
+        return doesContainDigit;
+    }
+}
